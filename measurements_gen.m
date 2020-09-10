fa0066ef9b2b1a2c9cb52c810fc1723ecfbbf925
asalline <66238266+asalline@users.noreply.github.com>
2020-09-10 16:19:13 +0300

    Delete measurements_gen.m

--- measurements_gen.m
+++
@@ -1,9 +0,0 @@
-function measurements = measurements_gen(original_rho, GGB_new, num_of_states, qudits)
-    clear measurements
-    coef = sqrt((num_of_states^qudits)/(2^qudits)*((num_of_states^qudits)-1));
-    for k = 1:length(GGB_new)
-        measurements(k) = trace(GGB_new{k} * original_rho);
-    end
-%     measurements = coef * measurements;
-end
-% sqrt(num_of_states/2*(num_of_states-1))
